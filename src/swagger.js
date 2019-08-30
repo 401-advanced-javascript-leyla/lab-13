13a2523c7475bb8979286f5879cbeb18d24f3c0a
leyla li <leylali@qq.com>
2019-08-30 13:25:10 -0700

    done with swagger

--- src/swagger.js
+++ src/swagger.js
@@ -22,7 +22,7 @@ let options = {
     },
   },
   basedir: __dirname, //app absolute path
-  files: ['/auth/router'], //Path to the API handle folder
+  files: ['./auth/router.js'], //Path to the API handle folder
 };
 expressSwagger(options);
 swaggerServer.listen(3333);
